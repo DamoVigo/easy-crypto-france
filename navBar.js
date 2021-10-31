c1921569758b4fd94ba6750a921ec6e585589e97
Damien Vigouroux <damo@vigo@gmail.com>
2021-10-31 17:38:50 -0400

    update final

--- navBar.js
+++ navBar.js
@@ -1,6 +1,6 @@
 const navLinks = document.querySelectorAll('.nav-link')
 const menuToggle = document.getElementById('navbarNavAltMarkup')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false})
 navLinks.forEach((e) => {
     e.addEventListener('click', () => { bsCollapse.toggle() })
 })
